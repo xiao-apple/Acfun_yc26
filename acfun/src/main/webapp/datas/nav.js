e30428a29d66a5550d69cf40b105c50a8d00c097
fy-cloud <295727233@qq.com>
2017-02-18 20:31:38 +0800

    layui--后台界面

---
+++ acfun/src/main/webapp/datas/nav.js
@@ -0,0 +1,63 @@
+var navs = [{
+	"title": "基本设置",
+	"icon": "fa-navicon",
+	"spread": false,
+	"children": [{
+		"title": "添加管理员",
+		"icon": "&#xe63c;",
+		"href": ""
+	}, {
+		"title": "发布公告",
+		"icon": "&#xe63c;",
+		"href": ""
+	}, {
+		"title": "系统日志",
+		"icon": "&#xe63c;",
+		"href": ""
+	}]
+}, {
+	"title": "用户",
+	"icon": "fa-navicon",
+	"spread": false,
+	"children": [{
+		"title": "用户列表",
+		"icon": "fa-address-book",
+		"href": ""
+	}, {
+		"title": "封禁用户列表",
+		"icon": "fa-address-book",
+		"href": ""
+	}]
+}, {
+	"title": "资源",
+	"icon": "fa-navicon",
+	"spread": false,
+	"children": [{
+		"title": "资源审核",
+		"icon": "fa-check-square-o",
+		"href": ""
+	}, {
+		"title": "资源下架",
+		"icon": "fa-check-square-o",
+		"href": ""
+	}, {
+		"title": "资源推荐",
+		"icon": "fa-check-square-o",
+		"href": ""
+	}]
+}, {
+	"title": "评论",
+	"icon": "fa-navicon",
+	"href": "",
+	"spread": false,
+	"children": [{
+		"title": "评论删除",
+		"icon": "&#xe63c;",
+		"href": ""
+	}]
+}, {
+	"title": "AcFun",
+	"icon": "&#xe609;",
+	"href": "http://www.acfun.cn/",
+	"spread": false
+}];
